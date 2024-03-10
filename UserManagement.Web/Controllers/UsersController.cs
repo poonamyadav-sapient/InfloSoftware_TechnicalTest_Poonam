2c8b3d65d4b1af226f467591468ab85591d333e5
Poonam Yadav <53041223+poonamyadav-sapient@users.noreply.github.com>
2024-03-10 14:13:33 +0000

    Added User page, set screen for View,Edit and Delete

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using UserManagement.Data;
+using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
 
@@ -8,7 +10,12 @@ namespace UserManagement.WebMS.Controllers;
 public class UsersController : Controller
 {
     private readonly IUserService _userService;
-    public UsersController(IUserService userService) => _userService = userService;
+    private readonly DataContext _dataContext;
+    public UsersController(IUserService userService, DataContext dataContext)
+    {
+        _userService = userService;
+        _dataContext = dataContext;
+    }
 
     [HttpGet]
     public ViewResult List()
@@ -71,5 +78,133 @@ public class UsersController : Controller
         };
 
         return View("List", model);
+    }
+
+    [HttpGet("add")]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(UserListItemViewModel userListItemViewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            var newUser = new User
+            {
+                Forename = userListItemViewModel.Forename!,
+                Surname = userListItemViewModel.Surname!,
+                Email = userListItemViewModel.Email!,
+                DateofBirth = userListItemViewModel.DateofBirth,
+                IsActive = userListItemViewModel.IsActive
+            };
+            _dataContext.Add(newUser);
+            _dataContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+        return View(userListItemViewModel);
+    }
+
+    [HttpGet("View/{id}")]
+    public IActionResult View(int id)
+    {
+        var user = _userService.GetAll().FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var viewModel = new UserListItemViewModel
+        {
+            Id = user.Id,
+            Forename = user.Forename,
+            Surname = user.Surname,
+            Email = user.Email,
+            DateofBirth = user.DateofBirth,
+            IsActive = user.IsActive
+
+        };
+        return View(viewModel);
+    }
+
+    [HttpGet("Edit/{id}")]
+    public IActionResult Edit(int id)
+    {
+        var user = _userService.GetAll().FirstOrDefault(x => x.Id == id);
+        if(user == null)
+        {
+            return NotFound(nameof(user));
+        }
+
+        var viewModel = new UserListItemViewModel
+        {
+            Id = user.Id,
+            Forename = user.Forename,
+            Surname = user.Surname,
+            Email = user.Email,
+            DateofBirth = user.DateofBirth,
+            IsActive = user.IsActive
+        };
+        return View(viewModel);
+    }
+    [HttpPost("Edit/{id}")]
+    public IActionResult Edit(int id, UserListItemViewModel model)
+    {
+        if(!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var user = _userService.GetAll().FirstOrDefault(x => x.Id == id);
+        if(user == null)
+        {
+            return NotFound();
+        }
+
+        //Update User Properties
+        user.Forename = model.Forename!;
+        user.Surname = model.Surname!;
+        user.Email = model.Email!;
+        user.DateofBirth = model.DateofBirth!;
+        user.IsActive = model.IsActive;
+
+        _dataContext.Update(user);
+        return RedirectToAction("List");
+    }
+    [HttpGet("Delete/{id}")]
+    public IActionResult Delete(int id)
+    {
+        var user = _userService.GetAll().FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound(nameof(user));
+        }
+
+        var viewModel = new UserListItemViewModel
+        {
+            Id = user.Id,
+            Forename = user.Forename,
+            Surname = user.Surname,
+            Email = user.Email,
+            DateofBirth = user.DateofBirth,
+            IsActive = user.IsActive
+        };
+        return View(viewModel);
+    }
+    [HttpPost("Delete/{id}")]
+    public IActionResult ConfirmDelete(int id)
+    {
+
+        var user = _userService.GetAll().FirstOrDefault(x => x.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+          
+        _dataContext.Delete(user);
+        return RedirectToAction("List");
     }
 }
